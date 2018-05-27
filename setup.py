e2b854dab799f3843aa1df84e2a96a552dab965b
mozog226125 <mozog226125@e-science.pl>
2018-05-28 00:02:48 +0100

    change setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ZTD_lib',
+    name='libraryZTD',
     version='0.1',
     packages=find_packages(exclude=['calculations*', 'FileManager*', 'StructureFile*']),
     description='Library to calculate ZTD',
