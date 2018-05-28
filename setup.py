aa292e53ba47d2ef622a8115c8b168510e254628
mozog226125 <mozog226125@e-science.pl>
2018-05-28 07:19:27 +0100

    setup

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='libraryZTD',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
     description='Library to calculate ZTD',
     long_description=open('README.md').read(),
-    url='https://github.com/matozog/libraryZTD.git',
+    url='https://github.com/matozog/libraryZTD',
     author='Mateusz Ozog',
     author_email='226125@student.pwr.edu.pl'
 )
